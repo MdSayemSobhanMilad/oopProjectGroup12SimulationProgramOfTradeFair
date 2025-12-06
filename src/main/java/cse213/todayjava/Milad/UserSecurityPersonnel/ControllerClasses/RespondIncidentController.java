9b6f354055845c77f8ee1b9643500555ca8b8a8f
MdSayemSobhanMilad <sayemmilad@gmail.com>
2025-12-06 22:14:21 +0600

    almost

--- src/main/java/cse213/todayjava/Milad/UserSecurityPersonnel/ControllerClasses/RespondIncidentController.java
+++ src/main/java/cse213/todayjava/Milad/UserSecurityPersonnel/ControllerClasses/RespondIncidentController.java
@@ -1,4 +1,4 @@
-package cse213.todayjava.Milad.UserMaintenanceStaff.ControllerClasses;
+package cse213.todayjava.Milad.UserSecurityPersonnel.ControllerClasses;
 
 import cse213.todayjava.Milad.UserMaintenanceStaff.MaintenanceReport;
 import cse213.todayjava.SceneSwitcher;
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RepairController {
+public class RespondIncidentController {
 
     @FXML
     private TableColumn<MaintenanceReport, LocalDate> generateDateTC;
